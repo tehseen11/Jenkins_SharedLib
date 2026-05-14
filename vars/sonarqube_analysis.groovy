f01d1ce91b0db2b7cb2030858cea4ba94c5e2e2b
Tehseen Nayeem Khan <127304318+tehseen11@users.noreply.github.com>
2026-05-14 16:20:18 +0530

    Refactor SonarQube analysis script for scanner tool

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,14 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-  }
+
+    def scannerHome = tool 'Sonar'
+
+    withSonarQubeEnv("${SonarQubeAPI}") {
+
+        sh """
+        ${scannerHome}/bin/sonar-scanner \
+        -Dsonar.projectName=${Projectname} \
+        -Dsonar.projectKey=${ProjectKey} \
+        -X
+        """
+    }
 }
